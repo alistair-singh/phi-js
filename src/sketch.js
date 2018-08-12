7b4af65d9423073c4788ad47a9febad8ab1eda0b
Alistair Singh <alistair.singh7@gmail.com>
2018-08-12 20:30:56 +0200

    fixed painting

--- src/sketch.js
+++ src/sketch.js
@@ -16,8 +16,8 @@ var convexHull = (pos, scale_, rotation, center, verticies, indicies) => {
     ];
   this.indicies = indicies || [0, 1, 2, 0, 3, 2];
 
-  const pointX = i => this.verticies[i].x * this.scale_ + this.pos.x;
-  const pointY = i => this.verticies[i].y * this.scale_ + this.pos.y;
+  const worldX = i => ((this.verticies[i].x * this.scale_ * Math.cos(this.rotation)) - (this.verticies[i].y * this.scale_ * Math.sin(this.rotation))) + this.pos.x;
+  const worldY = i => ((this.verticies[i].x * this.scale_ * Math.sin(this.rotation)) + (this.verticies[i].y * this.scale_ * Math.cos(this.rotation))) + this.pos.y;
   return {
     position: () => this.pos,
     rotation: () => this.rotation,
@@ -26,13 +26,13 @@ var convexHull = (pos, scale_, rotation, center, verticies, indicies) => {
       image(crateImage, -1, -1, 2, 2);
     },
     aabb: (tolerance = 0.2) => {
-      let minX = pointX(0);
+      let minX = worldX(0);
       let maxX = minX;
-      let minY = pointY(0);
+      let minY = worldY(0);
       let maxY = minY;
       for (let i = 1; i < this.verticies.length; i++) {
-        const x = pointX(i);
-        const y = pointY(i);
+        const x = worldX(i);
+        const y = worldY(i);
         minX = Math.min(minX, x);
         minY = Math.min(minY, y);
         maxX = Math.max(maxX, x);
@@ -45,12 +45,12 @@ var convexHull = (pos, scale_, rotation, center, verticies, indicies) => {
     triangles: (observer) => {
       for (let i = 0; i < this.indicies.length - 2; i += 3) {
         observer(
-          pointX(this.indicies[i]),
-          pointY(this.indicies[i]),
-          pointX(this.indicies[i + 1]),
-          pointY(this.indicies[i + 1]),
-          pointX(this.indicies[i + 2]),
-          pointY(this.indicies[i + 2])
+          worldX(this.indicies[i]),
+          worldY(this.indicies[i]),
+          worldX(this.indicies[i + 1]),
+          worldY(this.indicies[i + 1]),
+          worldX(this.indicies[i + 2]),
+          worldY(this.indicies[i + 2])
         );
       }
     }
@@ -59,32 +59,37 @@ var convexHull = (pos, scale_, rotation, center, verticies, indicies) => {
 
 function setup() {
   createCanvas(480, 480);
-  hulls = [convexHull(createVector(200, 200), 100)];
+  hulls = [convexHull(createVector(240, 240), 50, PI / 1)];
   crateImage = loadImage("crate.jpg");
 }
 
+var showModel = true;
 var showHull = true;
 var showAABB = true;
 function draw() {
   background(200);
   hulls.forEach(h => {
-    push();
-    rotate(this.rotation);
-    translate(this.pos.x, this.pos.y);
-    scale(this.scale_);
-    h.draw();
-    pop();
+    if (showModel) {
+      push();
+      translate(this.pos.x, this.pos.y);
+      rotate(this.rotation);
+      scale(this.scale_);
+      h.draw();
+      pop();
+    }
 
-    push();
     if (showHull) {
+      push();
       stroke(0, 255, 0);
       h.triangles((x0, y0, x1, y1, x2, y2) => {
         line(x0, y0, x1, y1);
         line(x1, y1, x2, y2);
         line(x2, y2, x0, y0);
       });
+      pop();
     }
     if (showAABB) {
+      push();
       const aabb = h.aabb(2);
       stroke(255, 0, 0);
       line(aabb[0].x, aabb[0].y, aabb[0].x, aabb[1].y);
@@ -92,7 +97,7 @@ function draw() {
 
       line(aabb[1].x, aabb[0].y, aabb[1].x, aabb[1].y);
       line(aabb[0].x, aabb[1].y, aabb[1].x, aabb[1].y);
+      pop();
     }
-    pop();
   });
 }
